b50b1264786c5202b45a5e3ccd1527411d653aa6
murad <muradkv@gmail.com>
2019-03-23 22:42:39 +0300

    Add LogInViewController
    With button, textfields, background and segue from WelcomeViewController

---
+++ Flash Chat/Controller/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  Flash Chat
+//
+//  Created by murad on 23/03/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
