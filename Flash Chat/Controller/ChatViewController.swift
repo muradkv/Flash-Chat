14438fce9df879ac0941efd661cf014e3860b3a1
murad <muradkv@gmail.com>
2019-03-25 23:09:34 +0300

    Add IBOutlets and IBActions to ChatViewController

--- Flash Chat/Controller/ChatViewController.swift
+++ Flash Chat/Controller/ChatViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var messageTableView: UITableView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendPressed(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func logOutPressed(_ sender: Any) {
+    }
+    
 }
