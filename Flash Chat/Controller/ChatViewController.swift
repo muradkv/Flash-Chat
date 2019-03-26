46f869f88bf8ef2aee680406e5b2d8718844781e
murad <muradkv@gmail.com>
2019-03-26 22:30:04 +0300

    Add Log out (Sign out) method to rootViewController (WelcomeViewController)

--- Flash Chat/Controller/ChatViewController.swift
+++ Flash Chat/Controller/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
@@ -25,6 +26,13 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func logOutPressed(_ sender: Any) {
+        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch {
+            print(error)
+        }
     }
     
 }
