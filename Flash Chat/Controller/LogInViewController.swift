effec83a866c7a8e8aec671642aa8e16b5e7ea7e
murad <muradkv@gmail.com>
2019-03-26 22:33:46 +0300

    Add IBOutlets and IBAction to LogInViewController

--- Flash Chat/Controller/LogInViewController.swift
+++ Flash Chat/Controller/LogInViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginPressed(_ sender: UIButton) {
     }
-    */
-
+    
 }
