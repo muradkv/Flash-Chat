6248ef326019c6cac3d127d3301a9fe0455cec83
murad <muradkv@gmail.com>
2019-03-27 19:31:00 +0300

    Add model class  Message that represents the blueprints for a message

---
+++ Flash Chat/Model/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by murad on 27/03/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+    var sender: String = ""
+    var messsageBody: String = "" 
+    
+}
