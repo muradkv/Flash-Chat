91b418e765922560f26a8aa513ee27377d6056e1
murad <muradkv@gmail.com>
2024-12-05 03:57:55 +0300

    Initial commit

---
+++ Flash Chat/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by murad on 05.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
